ee909fae1a31383e3fcc25ba99ddd0b30fd54c53
Zaid Omar <zaidomar@Zaids-iMac.local>
2019-12-19 02:20:43 -0500

    Created Login/Register Pages

--- src/components/auth/index.jsx
+++
@@ -1,2 +0,0 @@
-export { Login } from "./login/login";
-export { Register } from "./register/register";
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.jsx';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './assets/css/bootstrap.min.css'
-import './assets/css/agency.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
