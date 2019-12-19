b3925d36f5dd988261e0dcfd4108e416f9c2bb00
Zaid Omar <zaidomar@Zaids-iMac.local>
2019-12-19 03:42:14 -0500

    Created node login backend api calls and re created user models. Also added user session models

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -10,10 +11,33 @@ const userSchema = new Schema({
         trim: true,
         minlength: 3
     },
-},  {
-    timestamps: true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    password: {
+        type: String,
+        default: '',
+        required: true,
+        unique: true,
+        minlength: 3
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+}
 
-module.exports = User;
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User', userSchema);
+
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-let User = require('../models/user.model');
+const User = require('../models/user.model');
+const UserSession = require('../models/userSesion.model');
 
 router.route('/').get((req, res) => {
     User.find()
@@ -8,13 +9,194 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/add').post((req, res) => {
-    const username = req.body.username;
+    const { body } = req;
+    const {
+      username,
+      password
+    } = body;
 
-    const newUser = new User({username});
+    let {
+      email
+    } = body;
 
-    newUser.save()
-      .then(() => res.json('User Added!'))
-      .catch(err => res.status(400).json('Error: ' + err));
+    if(!username) {
+      return res.send({
+        success: false,
+        message: 'Error: Username cannot be blank'
+      });
+    }
+    if(!email) {
+      return res.send({
+        success: false,
+        message: 'Error: email cannot be blank'
+      });
+    }
+    if(!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank'
+      });
+    }
+
+    email = email.toLowerCase();
+
+  User.find({
+      email: email
+    }, (err, previousUsers) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error'
+        });
+      } else if (previousUsers.length > 0) {
+        return res.send({
+          success: false,
+          message: 'Error: Account already exists'
+        });
+      }
+      // Save new User
+      const newUser = new User();
+
+      newUser.email = email;
+      newUser.username = username;
+      newUser.password = newUser.generateHash(password);
+      newUser.save((err, user) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: 'Error: Server error'
+          });
+        }
+        return res.send({
+          success: true,
+          message: 'Signed up'
+        });
+      });
+    });
 });
+
+router.route('/signin').post((req, res, next) => {
+  const { body } = req;
+  const {
+    password
+  } = body;
+
+  let {
+    email
+  } = body;
+
+  if(!email) {
+    return res.send({
+      success: false,
+      message: 'Error: email cannot be blank'
+    });
+  }
+  if(!password) {
+    return res.send({
+      success: false,
+      message: 'Error: Password cannot be blank'
+    });
+  }
+
+  email = email.toLowerCase();
+  User.find({
+    email: email
+  }, (err, users) => {
+    if (err) {
+      return res.send({
+        success: false,
+        message: 'Error: server error'
+      });
+    }
+    if (users.length != 1) {
+      return res.send({
+        success: false,
+        message: 'Error: Invalid'
+      });
+    }
+
+    const user = users[0];
+    if (!user.validPassword(password)) {
+      return res.send({
+        success: false,
+        message: 'Error: Invalid Password'
+      });
+    }
+
+    // Correct the user
+    const userSession = new UserSession();
+    userSession.userId - user._id;
+    userSession.save((err, doc) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: server error'
+        });
+      }
+
+      return res.send({
+        success: true,
+        message: 'Valid sign in',
+        token: doc._id
+      });
+    });
+  });
+});
+
+router.route('/verify').get((req, res, next) => { 
+    // Get Token
+    const { query } = req;
+    const { token } = query;
+    // ?token = test
+    // Verify token is unique
+    UserSession.find({
+      _id: token,
+      isDeleted: false
+    }, (err, sessions) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error'
+        });
+      }
+      if (sessions.length != 1) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid'
+        })
+      }
+      else {
+        return res.send({
+          success: true,
+          message: 'Valid'
+      });
+    };
+  });
+});
+    
+router.route('/logout').get((req, res, next) => { 
+    // Get Token
+    const { query } = req;
+    const { token } = query;
+    // ?token = test
+    // Verify token is unique
+    UserSession.findOneAndUpdate({
+      _id: token,
+      isDeleted: false
+    }, {
+      $set:{isDeleted:true}
+    }, null, (err) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: Server error'
+        });
+      }
+      return res.send({
+        success: true,
+        message: 'Logged Out'
+      });
+    });
+  });
 
 module.exports = router;
