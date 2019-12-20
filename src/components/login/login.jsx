058bb7cdf8442568c86832805d0bf302d7fb85da
Zaid Omar <zaidomar@Zaids-iMac.local>
2019-12-19 19:34:20 -0500

    Added tokenization and elements to to help fetch and persist state

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,7 +2,44 @@ import React from "react";
 import loginImg from "../../login.svg";
 
 export class Login extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+     };
+  }
+
+  change = (e) => {
+      this.setState({
+          [e.target.name]: e.target.value
+      });
+  };
+
+  createRequest(opts) {
+    fetch('https://api.github.com/gists', {
+      method: 'post',
+      body: JSON.stringify(opts)
+    }).then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      console.log('Signed in');
+    });
+    console.log(this.state.username);
+    console.log(this.state.password);
+  }
+
+  submitLogin(e){
+    e.preventDefault();
+    var content = document.querySelector('textarea').value;
+    if (content) {
+      this.createRequest({
+        username: this.state.username,
+        password: this.state.password
+      });
+    }
+  }
+
   render() {
     return (
       <div className="base-container" ref={this.props.containerRef}>
@@ -14,16 +51,16 @@ export class Login extends React.Component {
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <input type="username" name="username" placeholder="username" value={this.state.username} onChange={e => this.change(e)}/>
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              <input type="password" name="password" placeholder="password" value={this.state.password} onChange={e => this.change(e)}/>
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick={e => this.submitLogin(e)}>
             Login
           </button>
         </div>
