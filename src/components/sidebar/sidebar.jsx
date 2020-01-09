c8851e6b7edd84964090446cd22f7c38ff982a4a
Zaid Omar <zaidomar@Zaids-iMac.local>
2020-01-08 20:11:21 -0500

    Created and Started on sidebar component

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
+import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
-function Sidebar() {
-  return null
+export class Sidebar extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      selected: 'home',
+      loggedOut: false
+    };
+  }
+  onSelect = (selected) => {
+    this.setState({ selected: selected });
+  };
+
+  isLoggedOut() {
+    this.setState({
+      loggedOut: true
+    })
+    return this.state.loggedOut;
+  }
+
+  render() {
+    if (this.selected === 'logout')
+    {
+      this.isLoggedOut();
+    }
+    return (
+    <SideNav
+    onSelect={this.onSelect}
+    >
+      <SideNav.Toggle />
+      <SideNav.Nav defaultSelected="home">
+          <NavItem eventKey="home">
+              <NavIcon>
+                  <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+              </NavIcon>
+              <NavText>
+                  Home
+              </NavText>
+          </NavItem>
+          <NavItem eventKey="logout">
+              <NavIcon>
+                  <i className="fa fa-sign-out" style={{ fontSize: '1.75em' }} />
+              </NavIcon>
+              <NavText>
+                  Logout
+              </NavText>
+          </NavItem>
+      </SideNav.Nav>
+  </SideNav>
+    )
+  }
 }
 
 export default Sidebar
