bde679c31bdacf7a23e821b8928d63393df1d3b6
Zaid <zaid@Zaids-MacBook-Pro.local>
2020-02-13 23:28:14 -0500

    made more changes to sidebar component and styling for spacing between navigation links

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -22,10 +22,16 @@ export class Home extends React.Component {
 
   render() {
     return (
-      <div className="home-container">
+      <div className="home">
         <Sidebar token={this.state.token}/>
+        <div className="home-container">
+          <div className="home-header">
+            <h1 className="home-header">
+                HELLO
+            </h1>
+          </div>
+        </div>
       </div>
-        
     );
   }
 }
